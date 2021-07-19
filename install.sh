57a04411a96010e09e8fa0ce49a6f33ea3faf682
hoangdn6 <hoangdn6@viettel.com.vn>
2021-07-19 17:04:50 +0700

    Add sshconfig

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-for folder in "tmux" "nvim" "p10k" "zshrc" 
+for folder in "tmux" "nvim" "p10k" "zshrc" "ssh"
 do
 	stow --target=$HOME -D --verbose=1 $folder
 	stow --target=$HOME --verbose=1 $folder
